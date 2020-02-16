9fe94f9eec3316fba4de68fa56bd8c101969466e
maureen mckenna <Mackenzie@maureens-MacBook-Air.local>
2020-02-16 16:18:48 -0500

    sving changes

--- src/components/header/Title.js
+++ src/components/header/Title.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Title extends Component {
   render() {
     return (
-      <div className="display-4 navbar bg-primary text-white pl-5">Noted.</div>
-    );
-  }
+
+  <>
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">NOTED</Navbar.Brand>
+    <Nav className="mr-auto">
+     <Nav.Link href="#home"></Nav.Link>
+    </Nav>
+<div class="btn-group open">
+ <a class="btn btn-primary" href="#"><i></i>Home</a>
+  <a class="btn btn-primary dropdown-toggle" data-toggle= "dropdown" href="#"><i></i>Settings</a>
+  <a class="btn btn-primary" href="#"><i></i>Logout</a>
+  <ul class="dropdown-menu">
+    <li><a href="#"><i></i> Edit Profile</a></li>
+    <li><a href="#"><i></i> Change Password</a></li>
+    <li><a href="#"><i></i> Account Settings</a></li>
+    <li><a href="#"><i></i> Privacy Settings</a></li>
+    <li><a href="#"><i class="i"></i> Logout</a></li>
+  </ul>
+</div>
+  </Navbar>
+  </>
+//
+//   }
+//   title="Button with icon component"
+// />
+//      <button type="button" class="btn btn-primary"><i class="fas fa-cog"></i> Settings</button>
+);
+}
 }
 
 export default Title;
