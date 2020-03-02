c11fab174724d7f60e75cef0b98c0654ae924cd4
maureen mckenna <Mackenzie@maureens-MacBook-Air.local>
2020-03-02 15:46:18 -0500

    changes

--- src/components/header/Title.js
+++ src/components/header/Title.js
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
-import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component } from 'react';
+import { Button, NavDropdown, Navbar, Nav, NavItem } from 'react-bootstrap';
+import { MenuItem, DropdownMenu } from 'react-bootstrap-dropdown-menu';
 
 class Title extends Component {
+
   render() {
     return (
-
-  <>
-  <Navbar bg="primary" variant="dark">
+      <>
+      <Navbar bg="primary" variant="dark">
     <Navbar.Brand href="#home">NOTED</Navbar.Brand>
     <Nav className="mr-auto">
-     <Nav.Link href="#home"></Nav.Link>
-    </Nav>
-<div class="btn-group open">
- <a class="btn btn-primary" href="#"><i></i>Home</a>
-  <a class="btn btn-primary dropdown-toggle" data-toggle= "dropdown" href="#"><i></i>Settings</a>
-  <a class="btn btn-primary" href="#"><i></i>Logout</a>
-  <ul class="dropdown-menu">
-    <li><a href="#"><i></i> Edit Profile</a></li>
-    <li><a href="#"><i></i> Change Password</a></li>
-    <li><a href="#"><i></i> Account Settings</a></li>
-    <li><a href="#"><i></i> Privacy Settings</a></li>
-  </ul>
-</div>
+     <Nav.Link href="#home">Home</Nav.Link>
+    <NavDropdown title="Settings" id="basic-nav-dropdown">
+        <NavDropdown.Item href="profile">Edit Profile</NavDropdown.Item>
+        <NavDropdown.Item href="profile">Change Password</NavDropdown.Item>
+        <NavDropdown.Item href="profile">Account Settings</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="profile">Logout</NavDropdown.Item>
+      </NavDropdown>
+</Nav>
   </Navbar>
   </>
 
 );
 }
 }
+
 
 export default Title;
