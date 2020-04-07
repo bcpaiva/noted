2415a39050d8f65b7c978086ff727b213bb82437
tpetrus <trevor.petrus@gmail.com>
2020-04-06 22:04:06 -0400

    Added Remove Class button

--- src/components/tabs/MyClasses/tab_components/ClassInfoModal.js
+++ src/components/tabs/MyClasses/tab_components/ClassInfoModal.js
@@ -2,66 +2,107 @@ import React, { Component } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import { withFirebase } from "../../../Firebase";
-
+import RemoveClassModal from "./RemoveClassModal";
 
 class ClassInfoModal extends Component {
- 
-  componentDidMount(){
+  constructor(props) {
+    super(props);
+    this.handleRmClassCancel = this.handleRmClassCancel.bind(this);
+    this.handleRmClassClick = this.handleRmClassClick.bind(this);
+    this.handleRmClassConfirm = this.handleRmClassConfirm.bind(this);
+  }
+
+  state = {
+    allImages: [],
+    className: this.props.theClass,
+    showDeleteClassModal: false,
+    classUid: null,
+  };
+
+  componentDidMount() {
     const self = this;
-
-    // Get class data from FB
-    this.props.firebase.fetchClassData(data => {
+    this.props.firebase.fetchClassData((data) => {
       let classData = data;
-
-      // Iterate through all of the class data until the correct match is found
-      for (let key in classData){
-        if (classData[key]["data"]["class_id"] == this.state.className){
-        let allNotes = classData[key]["data"]["notes"]
-        for (let note in allNotes){
-
-          // Get correct FB url to download the image 
-          let imageUrl = this.props.firebase.getNoteUrl(allNotes[note]);
-          imageUrl.getDownloadURL().then(function (url){
-
-            // Update the list of images that will be displayed in the Carousel
-            self.setState({allImages: self.state.allImages.concat(url)})
-           });
-        }
+      console.log("classdata", data);
+      for (let key in classData) {
+        if (classData[key]["data"]["class_id"] == this.state.className) {
+          this.setState({
+            classUid: key,
+          });
+          let allNotes = classData[key]["data"]["notes"];
+          for (let note in allNotes) {
+            let imageUrl = this.props.firebase.getNoteUrl(allNotes[note]);
+            imageUrl.getDownloadURL().then(function (url) {
+              self.setState({ allImages: self.state.allImages.concat(url) });
+            });
+          }
         }
       }
     });
-
-
-
-
   }
-  state = {
-    allImages: [],
-    className: this.props.theClass
-  };
+
+  // Handle click of Remove Class button
+  handleRmClassClick() {
+    this.setState({
+      showDeleteClassModal: true,
+    });
+  }
+
+  // Handle click Cancel button of Remove Class Modal
+  handleRmClassCancel() {
+    this.setState({
+      showDeleteClassModal: false,
+    });
+  }
+
+  // Handle click Confirm button of Remove Class Modal
+  handleRmClassConfirm() {
+    this.props.firebase.removeUserFromClass(
+      this.props.currentUser.uid,
+      this.state.classUid,
+      () => {
+        this.setState({
+          showDeleteClassModal: false,
+        });
+        this.props.onDeleteClass();
+      }
+    );
+  }
+
   render() {
-    const items = []
+    const items = [];
 
     for (let image in this.state.allImages) {
-        items.push( <div>
+      items.push(
+        <div>
           <img src={this.state.allImages[image]} />
-      </div>)
+        </div>
+      );
     }
     return (
       <React.Fragment>
+        {/* Show Delete Class Modal if Remove Class button clicked */}
+        {this.state.showDeleteClassModal ? (
+          <RemoveClassModal
+            classId={this.state.className}
+            onCancel={this.handleRmClassCancel}
+            onConfirm={this.handleRmClassConfirm}
+          />
+        ) : null}
+        {/** Show Class Info Modal */}
         <Modal show={true} onHide={this.props.onCancel}>
           <Modal.Header closeButton>
             <Modal.Title>Class Notes:</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-
-      <Carousel>        
-     {items}
-  </Carousel>
+            <Carousel>{items}</Carousel>
           </Modal.Body>
           <Modal.Footer>
+            <Button variant="danger" onClick={this.handleRmClassClick}>
+              Remove Class
+            </Button>
             <Button variant="secondary" onClick={this.props.onCancel}>
               Cancel
             </Button>
